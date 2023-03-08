f8130bfdf140e92f071521a2dec26f38636ec3d4
bibiefrat <bibi.efrat@gmail.com>
2023-03-08 02:21:16 +0200

    first commit

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,4 +1,4 @@
-call(Map config = [:]){
+def call(Map config = [:]){
     def scriptcontent = libraryResource  "org/linux/${config.name}"
     writeFile file:"${config.name}", text: scriptcontent
     sh "chmod a+x ./${config.name}"
